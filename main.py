742335c5dc760d9bc4b33c791cba84cc21fe39b1
17358761487 <2053752041@qq.com>
2025-10-14 20:33:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# 这个文件用于编写代码
+s = input("请输入一行字符：")
+letter_count = 0
+digit_count = 0
+space_count = 0
+other_count = 0
+for c in s:
+    if c.isalpha():
+        letter_count += 1
+    elif c.isdigit():
+        digit_count += 1
+    elif c.isspace():
+        space_count += 1
+    else:
+        other_count += 1
+print("中文字符：", letter_count)
+print("数字：", digit_count)
+print("空格：", space_count)
+print("其他角色：", other_count)
