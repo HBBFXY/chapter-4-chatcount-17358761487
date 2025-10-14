4ce6ac9fc0eeef8cbbae1c503e41e1f7e2ec08bb
17358761487 <2053752041@qq.com>
2025-10-14 20:40:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ for c in s:
         space_count += 1
     else:
         other_count += 1
-
-print(f"英文字符: {letter_count}")
-print(f"数字: {digit_count}")
-print(f"空格: {space_count}")
-print(f"其他字符: {other_count}")
+print("英文字符:", letter_count)
+print("数字:", digit_count)
+print("空格:", space_count)
+print("其他字符:", other_count)
